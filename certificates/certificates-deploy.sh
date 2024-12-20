f9f929096c4d31b8b65950c346740caac1f42059
Julien Dessaux <julien.dessaux@adyxax.org>
2024-12-20 16:52:35 +0100

    update certificates deployment script

--- certificates/certificates-deploy.sh
+++ certificates/certificates-deploy.sh
@@ -19,9 +19,12 @@ ssh ${SSHOPTS} root@yen.adyxax.org rcctl restart nginx
 #cp 'adyxax.org.crt' ~/git/adyxax/ansible/files/adyxax.org.fullchain
 #cp 'adyxax.org.key'  ~/git/adyxax/ansible/files/adyxax.org.key
 # TODO ansible make run
-scp ${SSHOPTS} "${KEY}" root@lore.adyxax.org:/usr/local/etc/nginx/adyxax.org.key
-scp ${SSHOPTS} "${CRT}" root@lore.adyxax.org:/usr/local/etc/nginx/adyxax.org.fullchain
+scp ${SSHOPTS} "${KEY}" root@lore.adyxax.org:/etc/nginx/adyxax.org.key
+scp ${SSHOPTS} "${CRT}" root@lore.adyxax.org:/etc/nginx/adyxax.org.fullchain
 ssh ${SSHOPTS} root@lore.adyxax.org service nginx reload
+scp ${SSHOPTS} "${KEY}" root@lore.adyxax.org:/etc/mail/adyxax.org.key
+scp ${SSHOPTS} "${CRT}" root@lore.adyxax.org:/etc/mail/adyxax.org.fullchain
+ssh ${SSHOPTS} root@lore.adyxax.org systemctl opensmtpd restart
 scp ${SSHOPTS} "${KEY}" root@kaladin.adyxax.org:/usr/local/etc/nginx/adyxax.org.key
 scp ${SSHOPTS} "${CRT}" root@kaladin.adyxax.org:/usr/local/etc/nginx/adyxax.org.fullchain
 ssh ${SSHOPTS} root@kaladin.adyxax.org service nginx reload
